afb64fda66291dc760270576ba62e61254757da5
Soumik Das <114335541+soumik1065@users.noreply.github.com>
2023-11-12 02:09:10 +0530

    Add files via upload

---
+++ 3. Program_2.c
@@ -0,0 +1,43 @@
+// Find the Transpose of a matrix
+
+#include<stdio.h>
+void main()
+{
+	int arr[50][50],m,n;
+
+	printf("Enter the size of row: ");
+	scanf("%d",&m);
+	printf("\nEnter the size of column: ");
+	scanf("%d",&n);
+
+	printf("\nEnter the elements in the array:\n");
+	for(int i=0; i<m; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			scanf("%d\n",&arr[i][j]);
+		}
+	}
+
+	printf("\nElements in the array are:\n");
+	for(int i=0; i<m; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("\nTranspose Matrix:\n");
+	for(int j=0; j<n; j++)
+	{
+		for(int i=0; i<m; i++)
+		{
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+
+
+}
